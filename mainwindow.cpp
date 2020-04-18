08cd6dd4ad21f2c5a5beb7f09b14df7b103e719e
Vincent-Zag <adza0810@colorado.edu>
2020-04-17 19:38:43 -0600

    Added Assets

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QTime>
+#include <QTimer>
+#include <QMovie>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+
     ui->setupUi(this);
+    this->setStyleSheet("background-color: white;");
+    QPixmap qp_title("/home/vincent/Documents/CSCI3010/HW5/WarioWare/assets/img/title_image.png");
+    ui->title_image->setPixmap(qp_title);
+    QPixmap qp_gif("/home/vincent/Documents/CSCI3010/HW5/WarioWare/assets/img/dancing_wario.gif");
+    ui->wario_gif->setPixmap(qp_gif);
+
+
+    QMovie *wario_dance = new QMovie("/home/vincent/Documents/CSCI3010/HW5/WarioWare/assets/img/wario_dancing.gif");
+    ui->wario_gif->setMovie(wario_dance);
+    wario_dance->start();
+
+
 }
 
 MainWindow::~MainWindow()
--- patience.cpp
+++ patience.cpp
@@ -6,6 +6,8 @@ patience::patience(QWidget *parent) :
     ui(new Ui::patience)
 {
     ui->setupUi(this);
+    QPixmap qp(":/home/vincent/Documents/CSCI3010/HW5/WarioWare/assets/img/patience_background.png");
+    ui->patience_background->setPixmap(qp);
 }
 
 patience::~patience()
