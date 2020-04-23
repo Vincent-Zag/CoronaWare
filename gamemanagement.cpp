2ae11a9f474fbe35df6ee3a8a112f64123389e77
Vincent-Zag <adza0810@colorado.edu>
2020-04-22 20:21:52 -0600

    Started Game Management Implementation

--- gamemanagement.cpp
+++ gamemanagement.cpp
@@ -2,5 +2,20 @@
 
 GameManagement::GameManagement()
 {
+   lives_ = 2;
+   score_ = 0;
+   time_ = new QTimer(this);
+
+}
+void GameManagement::DeleteLife(){
+
+}
+void GameManagement::Result(bool success){
+
+}
+void GameManagement::ShowScore(){
+
+}
+void GameManagement::ShowTimer(){
 
 }
--- gamemanagement.h
+++ gamemanagement.h
@@ -1,11 +1,22 @@
 #ifndef GAMEMANAGEMENT_H
 #define GAMEMANAGEMENT_H
 
+#include<QTimer>
 
 class GameManagement
 {
 public:
     GameManagement();
+    void DeleteLife();
+    void Result(bool success);
+    void ShowScore();
+    void ShowTimer();
+
+
+private:
+    int lives_;
+    int score_;
+    QTimer *time_;
 };
 
 #endif // GAMEMANAGEMENT_H
